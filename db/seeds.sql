fda504b8c3ac664e09bb2390f372e98785fc57be
Vanessa <vanessa.wiedeman.cs@gmail.com>
2020-01-18 14:03:44 -0600

    Project push (working on database display)

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', 'false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', 'false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', 'true');
